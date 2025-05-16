93eef51fc2339c02c3efef538654feefeac97b4d
Tianci.Xiong <tiancixiong@163.com>
2025-05-16 17:54:35 +0800

    feat(patterns): add new pattern images

--- website/src/patterns.ts
+++ website/src/patterns.ts
@@ -6,8 +6,8 @@ export const PATTERNS = [
     path: PATTERNS_PATH + 'animals.svg'
   },
   {
-    text: 'Astronaut Cats',
-    path: PATTERNS_PATH + 'astronaut_cats.svg'
+    text: 'Astronaut Cats (dense)',
+    path: PATTERNS_PATH + 'astronaut_cats_dense.svg'
   },
   {
     text: 'Beach',
@@ -30,7 +30,7 @@ export const PATTERNS = [
     path: PATTERNS_PATH + 'late_night_delight.svg'
   },
   {
-    text: 'Magic',
+    text: 'Magic (dense)',
     path: PATTERNS_PATH + 'magic.svg'
   },
   {
@@ -38,11 +38,11 @@ export const PATTERNS = [
     path: PATTERNS_PATH + 'math.svg'
   },
   {
-    text: 'Paris',
+    text: 'Paris (dense)',
     path: PATTERNS_PATH + 'paris.svg'
   },
   {
-    text: 'Games',
+    text: 'Games (dense)',
     path: PATTERNS_PATH + 'games.svg'
   },
   {
@@ -50,31 +50,151 @@ export const PATTERNS = [
     path: PATTERNS_PATH + 'snowflakes.svg'
   },
   {
-    text: 'Space',
+    text: 'Space (dense)',
     path: PATTERNS_PATH + 'space.svg'
   },
   {
-    text: 'Star Wars',
+    text: 'Star Wars (dense)',
     path: PATTERNS_PATH + 'star_wars.svg'
   },
   {
-    text: 'Sweets',
+    text: 'Sweets (dense)',
     path: PATTERNS_PATH + 'sweets.svg'
   },
   {
-    text: 'Tattoos',
+    text: 'Tattoos (dense)',
     path: PATTERNS_PATH + 'tattoos.svg'
   },
   {
-    text: 'Underwater World',
+    text: 'Underwater World (dense)',
     path: PATTERNS_PATH + 'underwater_world.svg'
   },
   {
-    text: 'Unicorns',
+    text: 'Unicorns (dense)',
     path: PATTERNS_PATH + 'unicorn.svg'
   },
   {
-    text: 'Zoo',
+    text: 'Zoo (dense)',
     path: PATTERNS_PATH + 'zoo.svg'
+  },
+  {
+    text: 'pattern',
+    path: PATTERNS_PATH + 'pattern.svg'
+  },
+  {
+    text: 'pattern-1',
+    path: PATTERNS_PATH + 'pattern-1.svg'
+  },
+  {
+    text: 'pattern-2',
+    path: PATTERNS_PATH + 'pattern-2.svg'
+  },
+  {
+    text: 'pattern-3  Kingkong VS Godzilla',
+    path: PATTERNS_PATH + 'pattern-3.svg'
+  },
+  {
+    text: 'pattern-4',
+    path: PATTERNS_PATH + 'pattern-4.svg'
+  },
+  {
+    text: 'pattern-5 Astronaut Cats (sparse)',
+    path: PATTERNS_PATH + 'pattern-5_astronaut_cats_sparse.svg'
+  },
+  {
+    text: 'pattern-6',
+    path: PATTERNS_PATH + 'pattern-6.svg'
+  },
+  {
+    text: 'pattern-7',
+    path: PATTERNS_PATH + 'pattern-7.svg'
+  },
+  {
+    text: 'pattern-8',
+    path: PATTERNS_PATH + 'pattern-8.svg'
+  },
+  {
+    text: 'pattern-9',
+    path: PATTERNS_PATH + 'pattern-9.svg'
+  },
+  {
+    text: 'pattern-10 Unicorns (sparse)',
+    path: PATTERNS_PATH + 'pattern-10.svg'
+  },
+  {
+    text: 'pattern-11 Space (sparse)',
+    path: PATTERNS_PATH + 'pattern-11.svg'
+  },
+  {
+    text: 'pattern-12',
+    path: PATTERNS_PATH + 'pattern-12.svg'
+  },
+  {
+    text: 'pattern-13 Zoo (sparse)',
+    path: PATTERNS_PATH + 'pattern-13.svg'
+  },
+  {
+    text: 'pattern-15 Star Wars (sparse)',
+    path: PATTERNS_PATH + 'pattern-15.svg'
+  },
+  {
+    text: 'pattern-16 Magic (sparse)',
+    path: PATTERNS_PATH + 'pattern-16.svg'
+  },
+  {
+    text: 'pattern-17',
+    path: PATTERNS_PATH + 'pattern-17.svg'
+  },
+  {
+    text: 'pattern-18',
+    path: PATTERNS_PATH + 'pattern-18.svg'
+  },
+  {
+    text: 'pattern-19 Underwater World (sparse)',
+    path: PATTERNS_PATH + 'pattern-19.svg'
+  },
+  {
+    text: 'pattern-20',
+    path: PATTERNS_PATH + 'pattern-20.svg'
+  },
+  {
+    text: 'pattern-21',
+    path: PATTERNS_PATH + 'pattern-21.svg'
+  },
+  {
+    text: 'pattern-22',
+    path: PATTERNS_PATH + 'pattern-22.svg'
+  },
+  {
+    text: 'pattern-23',
+    path: PATTERNS_PATH + 'pattern-23.svg'
+  },
+  {
+    text: 'pattern-24 Sweets (sparse)',
+    path: PATTERNS_PATH + 'pattern-24.svg'
+  },
+  {
+    text: 'pattern-25 Tattoos (sparse)',
+    path: PATTERNS_PATH + 'pattern-25.svg'
+  },
+  {
+    text: 'pattern-26 Games (sparse)',
+    path: PATTERNS_PATH + 'pattern-26.svg'
+  },
+  {
+    text: 'pattern-27 Forest',
+    path: PATTERNS_PATH + 'pattern-27.svg'
+  },
+  {
+    text: 'pattern-29 Paris (sparse)',
+    path: PATTERNS_PATH + 'pattern-29.svg'
+  },
+  {
+    text: 'pattern-30',
+    path: PATTERNS_PATH + 'pattern-30.svg'
+  },
+  {
+    text: 'pattern-31',
+    path: PATTERNS_PATH + 'pattern-31.svg'
   }
 ]
